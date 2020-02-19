44e4d1390fcd2bd25276955c3f18d23135ee582b
Kathryn Marshall <katiemarshall06@gmail.com>
2020-02-19 14:39:22 -0500

    complete lab 3

---
+++ labs/3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card/index.js'
+
+function App() {
+  return (
+    <Card content="This is a card!" />
+  );
+}
+
+export default App;
